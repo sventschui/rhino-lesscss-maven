047ed7dd32546b019bccee0849fda5554cc6e001
Sven Tschui <tschui@webmate.ch>
2014-04-17 11:08:37 +0200

    licensing, maven coordinates, formatting

--- rhino-lesscss-maven-plugin/src/main/java/ch/sventschui/lesscss/LessCssMojo.java
+++ rhino-lesscss-maven-plugin/src/main/java/ch/sventschui/lesscss/LessCssMojo.java
@@ -1,16 +1,15 @@
 package ch.sventschui.lesscss;
 
 /*
- * Copyright 2001-2005 The Apache Software Foundation.
+ * (C) Copyright 2014 Sven Tschui (http://eniu.ch/) and others.
  * 
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
- * the License. You may obtain a copy of the License at
+ * All rights reserved. This program and the accompanying materials are made available under the terms of the GNU Lesser
+ * General Public License (LGPL) version 2.1 which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/lgpl-2.1.html
  * 
- * http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
- * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
- * specific language governing permissions and limitations under the License.
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
  */
 
 import java.io.File;
@@ -61,7 +60,7 @@ public class LessCssMojo extends AbstractMojo {
         LessCssRunner runner = new LessCssRunner();
 
         for (Directory directory : directories) {
-            
+
             FileSet fileSet = directory.getInput();
             File inBase = new File(fileSet.getDirectory());
             File outBase = directory.getOutput();
@@ -69,26 +68,26 @@ public class LessCssMojo extends AbstractMojo {
             for (String filename : fileSetManager.getIncludedFiles(fileSet)) {
 
                 File input = new File(inBase, filename);
-                
-                if(buildContext != null && !buildContext.hasDelta(filename)) {
+
+                if (buildContext != null && !buildContext.hasDelta(filename)) {
                     continue;
                 }
 
                 File output = new File(outBase, filename.replaceAll("\\.less$", ".css"));
 
                 try {
-                    
+
                     getLog().info("Run " + input.getAbsolutePath());
-                    
+
                     runner.run(OptionsMerge.merge(options, directory.getOptions()), input, output);
                 } catch (OptionsMergeFailedException e) {
                     // TODO Auto-generated catch block
                     e.printStackTrace();
-                
+
                     continue;
                 }
-                
-                if(buildContext != null) {
+
+                if (buildContext != null) {
                     buildContext.refresh(output);
                 }
 
